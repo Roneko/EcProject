0e308731ce04787edb32084a3bbbd9d5d3cf078c
Mio Ogawa <Mio@test.com>
2020-04-01 12:47:14 +0900

    ルーティング修正

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,19 @@ use Illuminate\Support\Facades\Route;
     //管理者画面
     Route::get('/item', 'admin\ItemController@index')->name('item.index');
     Route::get('/item/create', 'admin\ItemController@create')->name('item.create');
-    Route::POST('/item/confirm', 'admin\ItemController@confirm')->name('item.confirm');
-    Route::POST('/item/store', 'admin\ItemController@store')->name('item.store');
     Route::GET('/item/edit', 'admin\ItemController@edit')->name('item.edit');
     Route::PATCH('/item/updata', 'admin\ItemController@updata')->name('item.updata');
     Route::DELETE('/item/destroy', 'admin\ItemController@destroy')->name('item.destroy');
+    Route::POST('/item/confirm', 'admin\ItemController@confirm')->name('item.confirm');
+    Route::POST('/item/store', 'admin\ItemController@store')->name('item.store');
+
     Route::get('/category', 'admin\CategoryController@index')->name('category.index');
-    Route::get('/category.create', 'admin\CategoryController@create')->name('category.create');
-    Route::post('/category.confirm', 'admin\CategoryController@confirm')->name('category.confirm');
-    Route::post('/category.store', 'admin\CategoryController@store')->name('category.store');
-    Route::get('/category.edit', 'admin\CategoryController@edit')->name('category.edit');
-    Route::patch('/category.updata', 'admin\CategoryController@updata')->name('category.updata');
-    Route::delete('/category.destroy', 'admin\CategoryController@destroy')->name('category.destroy');
+    Route::get('/category/create', 'admin\CategoryController@create')->name('category.create');
+    Route::get('/category/edit', 'admin\CategoryController@edit')->name('category.edit');
+    Route::patch('/category/updata', 'admin\CategoryController@updata')->name('category.updata');
+    Route::delete('/category/destroy', 'admin\CategoryController@destroy')->name('category.destroy');
+    Route::post('/category/confirm', 'admin\CategoryController@confirm')->name('category.confirm');
+    Route::post('/category/store', 'admin\CategoryController@store')->name('category.store');
     
     //マイページ
     Route::GET('/users/{user_id}', 'user\CartsController@show')->name('user.show');
