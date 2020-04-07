2552ff255c40db96375a7afcd2d1d71990221b09
Mio Ogawa <Mio@test.com>
2020-04-07 15:52:48 +0900

    マイグレーション追加

---
+++ database/migrations/2020_04_06_055826_create_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->BigIncrements('id');
+            $table->bigInteger('category_id');
+            $table->string('name');
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->string('path');
+            $table->text('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
